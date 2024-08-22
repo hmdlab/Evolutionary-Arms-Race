5516a27376e96b3913a502c03523e79b4769438f
Masato-Kosuge <102283812+Masato-Kosuge@users.noreply.github.com>
2024-08-22 13:57:43 +0900

    Add files via upload

--- script/download_rawdata.py
+++ script/download_rawdata.py
@@ -1,7 +1,6 @@
 import subprocess
 import os
 
-#subprocess.run('curl "https://drive.usercontent.google.com/download?id=1Ww9JjF6nrhPStu8M_VILuN_r1VcS7o9O&confirm=xxx" -o ../data.zip', shell=True)
 subprocess.run('wget "https://waseda.box.com/shared/static/fz2r7j4igktjdw1hf2oh24cs7ant85aa.zip" -O ../data.zip', shell=True)
 subprocess.run('unzip -d ../ -o ../data.zip', shell=True)
 
