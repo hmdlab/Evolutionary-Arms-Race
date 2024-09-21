060ea73c5831b5f2e3224b317626eca9e811bc4e
Masato-Kosuge <102283812+Masato-Kosuge@users.noreply.github.com>
2024-09-21 17:44:52 +0900

    Add files via upload

--- script/download_rawdata.py
+++ script/download_rawdata.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-subprocess.run('wget "https://waseda.box.com/shared/static/fz2r7j4igktjdw1hf2oh24cs7ant85aa.zip" -O ../data.zip', shell=True)
+subprocess.run('wget "https://waseda.box.com/shared/static/wkn8x878xwxtwh7lyshr12rn20o29mvd.zip" -O ../data.zip', shell=True)
 subprocess.run('unzip -d ../ -o ../data.zip', shell=True)
 
 print('finish downloading raw data')
